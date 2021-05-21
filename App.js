da41c3e374f14c602da9e8e71264cd4f0ba0a912
Nathan Rymarz <nrymarz@gmail.com>
2021-05-21 17:06:38 -0500

    added a container and buttons to main screen

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{paddingBottom:15}}>Some Recipe</Text>
+      <View style={[styles.recipeContainer]}>
+
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Left" color="brown" style={styles.leftButton}></Button>
+        <Button title="Right" color="brown"></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +21,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  recipeContainer:{
+    backgroundColor: "black",
+    height:"70%",
+    width: "80%"
+  },
+  buttonContainer:{
+    flexDirection:'row'
+  },
+  leftButton:{
+    marginRight:'auto'
+  }
 });
