bfc26cc8f80b2c480e3ffbd93c4039f699233a7e
Nathan Rymarz <nrymarz@gmail.com>
2021-06-02 19:37:42 -0500

    selected recipe link from recipe index page from foodnetwork site but now getting blocked by cors

--- scraper.js
+++ scraper.js
@@ -1,11 +1,22 @@
 import axios from 'axios'
 import {parse} from 'node-html-parser'
 
-const link = "https://www.foodnetwork.com/search/p/1/CUSTOM_FACET:RECIPE_FACET"
+const recipeIndex = "https://www.foodnetwork.com/search/p/1/CUSTOM_FACET:RECIPE_FACET"
+
+let dom = ''
 
 export default function findRecipe(){
-    let dom = ''
-    axios.get(link)
+    axios.get(recipeIndex)
         .then(res => dom = parse(res.data))
-        .then( () => console.log(dom))
+        .then(openRecipe)
+}
+
+function openRecipe(){
+    const recipe = dom.querySelector('h3 a')
+     axios.get(recipe.attrs.href)
+         .then(res => createRecipeObj(parse(res.data)))
+}
+
+function createRecipeObj(recipe){
+    console.log(recipe)
 }
