b918d7e17bc3100f72692d4dee0f67d142feeb6e
Nathan Rymarz <nrymarz@gmail.com>
2021-06-17 17:57:10 -0500

    changed styling for views on recipe page and imported Swipeable component

--- containers/Home.js
+++ containers/Home.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Button, FlatList, Image } from 'react-native';
 import findRecipe from '../scraper'
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 export default function HomePage({navigation, addRecipe}) {
 
@@ -51,32 +52,33 @@ export default function HomePage({navigation, addRecipe}) {
       />
     )
   }
-
-  console.log(recipe)
   
   return (
     <View style={styles.container}>
       <Button title ="Go to My Recipes" onPress={()=>navigation.navigate('My Recipes')}></Button>
-      <View style={styles.recipeContainer}>
-        <Text style={{textAlign:'center', paddingTop:12, fontWeight:"700", fontSize:15}}>{loading ? "Loading..." : recipe.title}</Text>
-        <Text style={{textAlign:'center', paddingTop:5}}>{loading ? "" : recipe.chef}</Text>
-        <Image style={{width:100, height:80}} source={recipe.image}/>
-        <Text style={{paddingTop:10, fontWeight:"bold"}}>Ingredients</Text>
-        <FlatList 
-          data={loading ? ["Loading..."] : recipe.ingredients} 
-          style={{backgroundColor:'lightgreen'}} 
-          renderItem={renderIngredients}
-          keyExtractor={(item,idx) => item + idx}
-        />
-        <Text style={{paddingTop:15, fontWeight:"bold"}}>Directions</Text>
-        <FlatList 
-          style={{backgroundColor:"pink"}} 
-          ItemSeparatorComponent={directionSeperator} 
-          data={loading ? ["Loading..."] : recipe.directions} 
-          renderItem={renderDirections}
-          keyExtractor={item => item}
-        />
-      </View>
+      <Swipeable style={{width:"100%",backgroundColor:"blue"}}
+        onSwipeableLeftOpen={handlePressLeft}>
+        <View style={styles.recipeContainer}>
+          <Text style={{textAlign:'center', paddingTop:12, fontWeight:"700", fontSize:15}}>{loading ? "Loading..." : recipe.title}</Text>
+          <Text style={{textAlign:'center', paddingTop:5}}>{loading ? "" : recipe.chef}</Text>
+          <Image style={{width:100, height:80}} source={recipe.image}/>
+          <Text style={{paddingTop:10, fontWeight:"bold"}}>Ingredients</Text>
+          <FlatList 
+            data={loading ? ["Loading..."] : recipe.ingredients} 
+            style={{backgroundColor:'lightgreen'}} 
+            renderItem={renderIngredients}
+            keyExtractor={(item,idx) => item + idx}
+          />
+          <Text style={{paddingTop:15, fontWeight:"bold"}}>Directions</Text>
+          <FlatList 
+            style={{backgroundColor:"pink"}} 
+            ItemSeparatorComponent={directionSeperator} 
+            data={loading ? ["Loading..."] : recipe.directions} 
+            renderItem={renderDirections}
+            keyExtractor={item => item}
+          />
+        </View>
+      </Swipeable>
       <View style={styles.buttonContainer}>
         <Button title="Left" color="brown" style={styles.button} onPress={handlePressLeft}></Button>
         <Button title="Right" color="brown" style={styles.button} onPress ={handlePressRight}></Button>
@@ -87,17 +89,17 @@ export default function HomePage({navigation, addRecipe}) {
   
 const styles = StyleSheet.create({
   container:{
-    flex: 1,
+    flex:1,
     backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
   recipeContainer:{
     backgroundColor: 'rgba(250,250,250,.85)',
-    height:"85%",
-    width: "90%"
+    flex:1
   },
   buttonContainer:{
+    height:60,
     width:"100%",
     flexDirection:'row',
     justifyContent:"space-between"
