d8f8048d007ea8e19a2963a9269962b803c8705d
Nathan Rymarz <nrymarz@gmail.com>
2021-06-28 20:37:57 -0500

    edited margins on recipeImage component

--- components/RecipeImage.js
+++ components/RecipeImage.js
@@ -13,17 +13,17 @@ export default function RecipeImage({click, recipe}){
           style={{height:400,width:"100%", resizeMode:'cover'}}
         />
       </TouchableHighlight>
-      <View>
+      <View style={{marginHorizontal:10}}>
           <Text style={{textAlign:'center', paddingTop:12, fontWeight:"700", fontSize:20}}>{title}</Text>
           <Text style={{textAlign:'center', paddingTop:8, fontSize:18}}>{chef}</Text>
       </View>
-      <View style={{marginTop:100, flexDirection:'row'}}>
-        <Image source={left} style={{width:80, height:50, marginLeft:10}}/>
-        <Image source={right} style={{width:80, height:50, marginLeft:'auto', marginRight:10}}/>
+      <View style={{marginTop:75, flexDirection:'row', marginHorizontal:20}}>
+        <Image source={left} style={{width:80, height:50}}/>
+        <Image source={right} style={{width:80, height:50, marginLeft:'auto'}}/>
       </View>
-      <View style={{marginTop:5, flexDirection:'row'}}>
-        <Text style={{marginLeft:20}}>Discard</Text>
-        <Text style={{marginLeft:'auto',marginRight:20}}>Save</Text>
+      <View style={{marginTop:5, flexDirection:'row', marginHorizontal:35}}>
+        <Text>Discard</Text>
+        <Text style={{marginLeft:'auto'}}>Save</Text>
       </View>
       <Text style={{marginLeft:'auto',marginRight:'auto',marginTop:'auto'}}>Click on the picture for recipe details.</Text>
     </>
