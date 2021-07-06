252256efbb404c61bd57e68ac0e9f86b2c1cea43
Nathan Rymarz <nrymarz@gmail.com>
2021-07-06 12:10:22 -0500

    changed styling and added pictures to MyRecipes component

--- containers/MyRecipes.js
+++ containers/MyRecipes.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, Image, TouchableOpacity } from 'react-native';
 
 
 export default function MyRecipes({navigation, recipes}){
     const renderRecipe = ({item}) =>{
         return(
-            <View style={{paddingTop:5}}>
-                <Text onPress={ () => navigation.navigate('Recipe Page',{recipe: item})}>{item.title} by {item.chef}</Text>
-            </View>
+            <TouchableOpacity style={{marginBottom:10, backgroundColor:"silver" }} onPress={ () => navigation.navigate('Recipe Page',{recipe: item})} >
+                <View style={{flex:1, flexDirection:'row'}}>
+                    <Image source={{uri: item.image}} style={{width:70,height:70,marginLeft:5,marginRight:10}}/>
+                    <View style={{alignContent:'center',justifyContent:"center"}}>
+                        <Text>{item.title}</Text>
+                        <Text>by: {item.chef}</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
         )
     }
 
@@ -23,4 +29,5 @@ export default function MyRecipes({navigation, recipes}){
 }
 
 const styles = StyleSheet.create({
+
 })
