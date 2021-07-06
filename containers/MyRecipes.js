848cd0c77db2655fe7541c332a78450b401598e2
Nathan Rymarz <nrymarz@gmail.com>
2021-07-06 12:19:34 -0500

    fixed text on MyRecipes component and added background color by index

--- containers/MyRecipes.js
+++ containers/MyRecipes.js
@@ -3,12 +3,12 @@ import { StyleSheet, Text, View, Button, FlatList, Image, TouchableOpacity } fro
 
 
 export default function MyRecipes({navigation, recipes}){
-    const renderRecipe = ({item}) =>{
+    const renderRecipe = ({item, index}) =>{
         return(
-            <TouchableOpacity style={{marginBottom:10, backgroundColor:"silver" }} onPress={ () => navigation.navigate('Recipe Page',{recipe: item})} >
+            <TouchableOpacity style={[styles.recipeWrapper, {backgroundColor: index%2===0 ? "rgb(210,210,210)" : "rgb(225,225,225)"}]} onPress={ () => navigation.navigate('Recipe Page',{recipe: item})} >
                 <View style={{flex:1, flexDirection:'row'}}>
-                    <Image source={{uri: item.image}} style={{width:70,height:70,marginLeft:5,marginRight:10}}/>
-                    <View style={{alignContent:'center',justifyContent:"center"}}>
+                    <Image source={{uri: item.image}} style={{width:70,height:70, marginRight:10}}/>
+                    <View style={{alignContent:'center',justifyContent:"center", width:"80%"}}>
                         <Text>{item.title}</Text>
                         <Text>by: {item.chef}</Text>
                     </View>
@@ -29,5 +29,8 @@ export default function MyRecipes({navigation, recipes}){
 }
 
 const styles = StyleSheet.create({
-
+    recipeWrapper:{
+        marginBottom:10,
+        marginHorizontal:10
+    }
 })
