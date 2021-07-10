5495736d4794ff7d702130e3ad23c685617cf7e0
Nathan Rymarz <nrymarz@gmail.com>
2021-07-10 12:56:04 -0500

    added maxHeight as animated value to recielistItem state and using to animate height to 0 when item is deleted

--- components/RecipeListItem.js
+++ components/RecipeListItem.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, Animated } from 'react-native';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 
-export default function RecipeListItem({recipe, index, deleteRecipe}){
-    const translateX = new Animated.Value(0)
+export default function RecipeListItem({navigation, recipe, index, deleteRecipe}){
 
-    function animatedDelete(){
-        Animated.timing(translateX,{
-            toValue: 400,
+    const [maxHeight, setMaxHeight] = useState(new Animated.Value(100))
+
+    const animatedDelete= ()=>{
+        Animated.timing(maxHeight,{
+            toValue: 0,
             duration: 400,
-            useNativeDriver:true
-        }).start(()=>deleteRecipe(recipe.id))
+            useNativeDriver:false
+        }).start(()=> deleteRecipe(recipe.id))
     }
 
     const renderLeftSwipe = (progress, dragX) =>{
@@ -20,25 +21,23 @@ export default function RecipeListItem({recipe, index, deleteRecipe}){
             extrapolate:'clamp'
         })
         return(
-            <Animated.View style={[styles.recipeWrapper,{backgroundColor:'rgb(250,0,0)', transform:[{translateX}]}]}>
-                <Text onPress={() => animatedDelete()} style={{paddingHorizontal:5,fontWeight:'bold',marginTop:'auto',marginBottom:'auto'}}>Delete Recipe</Text>
+            <Animated.View style={[styles.recipeWrapper,{backgroundColor:'rgb(250,0,0)',height:"100%",transform:[{translateX}]}]}>
+                <Text onPress={animatedDelete} style={{paddingHorizontal:5,fontWeight:'bold',marginTop:'auto',marginBottom:'auto'}}>Delete Recipe</Text>
             </Animated.View>
         )
     }
 
     return(
         <Swipeable renderLeftActions={renderLeftSwipe} friction={1.75}>
-            <Animated.View style={[styles.recipeWrapper, {backgroundColor: index%2===0 ? "rgb(210,210,210)" : "rgb(225,225,225)",transform:[{translateX}]}]}>
-            <TouchableOpacity onPress={ () => navigation.navigate('Recipe Page',{recipe: item})} >
-                <Animated.View style={{flex:1, flexDirection:'row'}}>
-                    <Image source={{uri: recipe.image}} style={{width:70,height:70, marginRight:10}}/>
-                    <View style={{alignContent:'center',justifyContent:"center", width:"80%"}}>
+            <TouchableOpacity onPress={ () => navigation.navigate('Recipe Page',{recipe})} >
+                <Animated.View style={[styles.recipeWrapper,{flex:1, flexDirection:'row', backgroundColor: index%2===0 ? "rgb(210,210,210)" : "rgb(225,225,225)", maxHeight}]}>
+                    <Image source={{uri: recipe.image}} style={{width:70,height:"100%", marginRight:10}}/>
+                    <View style={{alignContent:'center',justifyContent:"center", width:"80%",overflow:'hidden'}}>
                         <Text>{recipe.title}</Text>
                         <Text>by: {recipe.chef}</Text>
                     </View>
                 </Animated.View>
             </TouchableOpacity>
-            </Animated.View>
         </Swipeable>
     )
 }
@@ -46,6 +45,7 @@ export default function RecipeListItem({recipe, index, deleteRecipe}){
 
 const styles = StyleSheet.create({
     recipeWrapper:{
-        marginVertical: 2
+        height:70,
+        marginVertical: 1
     }
 })
--- containers/MyRecipes.js
+++ containers/MyRecipes.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { StyleSheet, View, FlatList} from 'react-native';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
+import React,{useState} from 'react';
+import { StyleSheet, View, FlatList, Animated} from 'react-native';
 import RecipeListItem from '../components/RecipeListItem'
 
-
 export default function MyRecipes({navigation, recipes, setRecipes}){
+
     function deleteRecipe(id){
         setRecipes(recipes.filter(r => r.id !== id))
     }
 
     const renderRecipe = ({item, index}) =>{
-        return <RecipeListItem recipe={item} index={index} deleteRecipe={deleteRecipe}/>
+        return <RecipeListItem navigation={navigation} recipe={item} index={index} deleteRecipe={deleteRecipe}/>
     }
 
     return(
