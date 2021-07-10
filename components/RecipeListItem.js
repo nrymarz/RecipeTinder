a9ef34e4a78ad204a3dd2083e2d5ec6ad973b74c
Nathan Rymarz <nrymarz@gmail.com>
2021-07-10 14:04:24 -0500

    changed around styling and removed unnesscary view from reicpeListItem

--- components/RecipeListItem.js
+++ components/RecipeListItem.js
@@ -21,7 +21,7 @@ export default function RecipeListItem({navigation, recipe, index, deleteRecipe}
             extrapolate:'clamp'
         })
         return(
-            <Animated.View style={[styles.recipeWrapper,{backgroundColor:'rgb(250,0,0)',height:"100%",transform:[{translateX}]}]}>
+            <Animated.View style={[{backgroundColor:'rgb(250,0,0)',transform:[{translateX}]}]}>
                 <Text onPress={animatedDelete} style={{paddingHorizontal:5,fontWeight:'bold',marginTop:'auto',marginBottom:'auto'}}>Delete Recipe</Text>
             </Animated.View>
         )
@@ -29,14 +29,12 @@ export default function RecipeListItem({navigation, recipe, index, deleteRecipe}
 
     return(
         <Swipeable renderLeftActions={renderLeftSwipe} friction={1.75}>
-            <TouchableOpacity onPress={ () => navigation.navigate('Recipe Page',{recipe})} >
-                <Animated.View style={[styles.recipeWrapper,{flex:1, flexDirection:'row', backgroundColor: index%2===0 ? "rgb(210,210,210)" : "rgb(225,225,225)", maxHeight}]}>
-                    <Image source={{uri: recipe.image}} style={{width:70,height:"100%", marginRight:10}}/>
-                    <View style={{alignContent:'center',justifyContent:"center", width:"80%",overflow:'hidden'}}>
-                        <Text>{recipe.title}</Text>
-                        <Text>by: {recipe.chef}</Text>
-                    </View>
-                </Animated.View>
+            <TouchableOpacity style={[styles.recipeWrapper, {backgroundColor: index%2===0 ? "rgb(210,210,210)" : "rgb(225,225,225)"}]} onPress={ () => navigation.navigate('Recipe Page',{recipe})} >
+                <Image source={{uri: recipe.image}} style={{width:70, height:70, marginRight:10}}/>
+                <View style={{alignContent:'center',justifyContent:"center", width:"82%"}}>
+                    <Text>{recipe.title}</Text>
+                    <Text>by: {recipe.chef}</Text>
+                </View>
             </TouchableOpacity>
         </Swipeable>
     )
@@ -45,7 +43,11 @@ export default function RecipeListItem({navigation, recipe, index, deleteRecipe}
 
 const styles = StyleSheet.create({
     recipeWrapper:{
+        flex:1,
+        flexDirection:'row',
         height:70,
         marginVertical: 1
     }
 })
+
+//<TouchableOpacity onPress={ () => navigation.navigate('Recipe Page',{recipe})} >
