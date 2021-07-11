37cb843699a09f6c88df6c0931602578b3f75fda
Nathan Rymarz <nrymarz@gmail.com>
2021-07-10 20:42:42 -0500

    changed recipeListItem so on delete the height is animated to 0 and removed translatingX when swiping to side

--- components/RecipeListItem.js
+++ components/RecipeListItem.js
@@ -1,42 +1,50 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Animated } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, Animated, Dimensions } from 'react-native';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 
+
 export default function RecipeListItem({navigation, recipe, index, deleteRecipe}){
+    const windowWidth = Dimensions.get('window').width
 
-    const [maxHeight, setMaxHeight] = useState(new Animated.Value(100))
+    const [height,setHeight] = useState(new Animated.Value(70))
 
-    const animatedDelete= ()=>{
-        Animated.timing(maxHeight,{
+    // const reverseWidth = width.interpolate({
+    //     inputRange:[120,windowWidth],
+    //     outputRange:[windowWidth,0]
+    // })
+
+    const animatedDelete=() => {
+        Animated.timing(height,{
             toValue: 0,
-            duration: 400,
+            duration: 200,
             useNativeDriver:false
         }).start(()=> deleteRecipe(recipe.id))
     }
 
     const renderRightSwipe = (progress, dragX) =>{
         const translateX = dragX.interpolate({
-            inputRange:[-108,0],
-            outputRange:[0,108],
+            inputRange:[-120,0],
+            outputRange:[0,120],
             extrapolate:'clamp'
         })
-        console.log(dragX)
         return(
-            <Animated.View style={[{backgroundColor:'rgb(250,0,0)',transform:[{translateX}]}]}>
-                <Text onPress={animatedDelete} style={{paddingHorizontal:5,fontWeight:'bold',marginTop:'auto',marginBottom:'auto'}}>Delete Recipe</Text>
+            <Animated.View style={[{marginVertical:1,backgroundColor:'rgb(250,0,0)', height,overflow:'hidden'}]}>
+                <Text onPress={animatedDelete} style={styles.deleteText}>Delete Recipe</Text>
             </Animated.View>
         )
     }
 
     return(
         <Swipeable renderRightActions={renderRightSwipe} friction={1.75}>
-            <TouchableOpacity style={[styles.recipeWrapper, {backgroundColor: index%2===0 ? "rgb(210,210,210)" : "rgb(225,225,225)"}]} onPress={ () => navigation.navigate('Recipe Page',{recipe})} >
-                <Image source={{uri: recipe.image}} style={{width:70, height:70, marginRight:10}}/>
-                <View style={{alignContent:'center',justifyContent:"center", width:"82%"}}>
-                    <Text>{recipe.title}</Text>
-                    <Text>by: {recipe.chef}</Text>
-                </View>
-            </TouchableOpacity>
+            <TouchableHighlight onPress={()=>navigation.navigate('Recipe Page',{recipe})} >
+                <Animated.View style={[styles.recipeWrapper, {height, backgroundColor:"rgb(225,225,225)"}]}>
+                    <Image source={{uri: recipe.image}} style={{width:70, height:70, marginRight:10}}/>
+                    <View style={{alignContent:'center',justifyContent:"center", width:windowWidth-90}}>
+                        <Text>{recipe.title}</Text>
+                        <Text>by: {recipe.chef}</Text>
+                    </View>
+                </Animated.View>
+            </TouchableHighlight>
         </Swipeable>
     )
 }
@@ -46,8 +54,15 @@ const styles = StyleSheet.create({
     recipeWrapper:{
         flex:1,
         flexDirection:'row',
-        marginVertical: 1
+        marginVertical: 1,
+        overflow:'hidden'
+    },
+    deleteText:{
+        marginLeft:'auto',
+        marginRight:'auto',
+        fontWeight:'bold',
+        marginTop:'auto',
+        marginBottom:'auto',
+        paddingHorizontal:5
     }
 })
-
-//<TouchableOpacity onPress={ () => navigation.navigate('Recipe Page',{recipe})} >
