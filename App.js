5c5184d872a443c2b7dcb920d7570624138dc3c9
Nathan Rymarz <nrymarz@gmail.com>
2022-01-24 12:41:42 -0600

    removed unneccessary variable stack inside App.js

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator()
-const Stack = createStackNavigator()
+
 
 export default function App() {
   const [recipes,setRecipes] = useState([])
